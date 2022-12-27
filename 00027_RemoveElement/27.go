bb5f5340fe1ece46b5b310f2069b3fd68cec2646
qianqn <qianqingnian@163.com>
2022-12-27 20:58:59 +0800

    add 00027

--- 00027_RemoveElement/27.go
+++ 00027_RemoveElement/27.go
@@ -1,5 +1,29 @@
 package removeelement
 
+// 2 2 3 3， 3
+// 3， 3
+// , 3
+// 0,1,2,2,3,0,4,2, 2
+
+/*
+执行用时： 0 ms , 在所有 Go 提交中击败了 100.00% 的用户
+内存消耗： 2 MB , 在所有 Go 提交中击败了 57.50% 的用户
+通过测试用例： 113 / 113
+*/
 func removeElement(nums []int, val int) int {
-	return 0
+	count := len(nums)
+	k := count
+	for i := 0; i < k; {
+		if nums[i] == val {
+			t := nums[i]
+			for j := i + 1; j < k; j++ {
+				nums[j-1] = nums[j]
+			}
+			k--
+			nums[k] = t
+		} else {
+			i++
+		}
+	}
+	return k
 }
---
+++ 00027_RemoveElement/27_test.go
@@ -0,0 +1,33 @@
+package removeelement
+
+import "testing"
+
+func TestRemoveElement(t *testing.T) {
+	var cases = []struct {
+		name       string
+		nums       []int
+		val        int
+		expectVal  int
+		expectNums []int
+	}{
+		{"case1", []int{3, 2, 2, 3}, 3, 2, []int{2, 2}},
+		{"case2", []int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5, []int{0, 1, 3, 0, 4}},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			k := removeElement(tt.nums, tt.val)
+			t.Logf("%+v, get:%d", tt, k)
+			if k != tt.expectVal {
+				t.Errorf("%+v, get:%d", tt, k)
+				return
+			}
+			for i := 0; i < k; i++ {
+				if tt.expectNums[i] != tt.nums[i] {
+					t.Errorf("%+v", tt)
+					return
+				}
+			}
+		})
+	}
+}
